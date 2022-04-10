7eaacce73cf9f503dc5b854ef038648234d53de7
Suxarik777 <gultcev@mail.ru>
2022-04-11 01:46:30 +0300

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
+
+//Console.WriteLine(numberA + numberB); вариант номер 1, положить формулу прямо в команду вывода на экран
+
+//int result = numberA + numberB;
+//Console.WriteLine(result);        вар. 2 это делаем переменную результат и уже потом эту переменную выводим в сообщение
