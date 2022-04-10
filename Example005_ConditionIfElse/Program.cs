3b7177b48cad9979d424d44ec369d6f9760333ec
Suxarik777 <gultcev@mail.ru>
2022-04-11 02:47:43 +0300

    Example 005 Condition If Else Cadd random

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //.ToLower преводит почти все буквы в маленькие
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
