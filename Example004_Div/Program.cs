ed88ba190917808ae33b95070b9f08a891845ce4
Suxarik777 <gultcev@mail.ru>
2022-04-11 02:53:04 +0300

    Example 004 Div + info about double & int  commit3

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,7 @@
 ﻿double numberA = 12;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
+
+// **int** number - целое число тип данных, контейнер (number название переменной для примера)
+
+//**double** numberB - контейнер позволяющий работать с вещественными числами (в смысле не целыми))
