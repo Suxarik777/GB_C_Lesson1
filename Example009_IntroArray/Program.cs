8ce52ff261d404f916a3a539a66cbb126928ef5e
Suxarik777 <gultcev@mail.ru>
2022-04-12 22:28:46 +0300

    Example009, запустили поиск большего числа через данные массива

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -7,13 +7,16 @@
 }
 
 // индексы       0   1   2   3   4   5   6  7    8
-int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
-array[0] = 12; //в нулевой индекс массива арэй запихиваем другое число 12
-Console.WriteLine(array[4]); //смотрим массив, вызываем число 15
+int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
 
 
 
-
+int result = MaxFunction(
+    MaxFunction(array[0], array[1], array[2]),
+    MaxFunction(array[3], array[4], array[5]),
+    MaxFunction(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
 
 
 
