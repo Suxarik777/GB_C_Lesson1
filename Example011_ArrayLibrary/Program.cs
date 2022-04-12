a6badcf3ade19d5e2ec7cef8b05992d9c1d4b7f8
Suxarik777 <gultcev@mail.ru>
2022-04-12 23:53:08 +0300

    Example011 Метод по поиску ПОСЛЕДНЕГО элемента массива, ищем 4 показываем где она находится

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) //void это такой тип данных который при функции не возвращает ничего
 {
     int length = collection.Length;
     int index = 0;
@@ -20,7 +20,29 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)// функция в которую приходит в качестве аргумента массив collection и аргумент find
+{
+    int count = collection.Length; //определяем количество элементов в массиве и заносим в переменую
+    int index = 0;
+    int position = 0; //создали переменную чтобы сохранять данные ниже
+
+    while (index < count) // то есть при помощи такой конструкции мы идём по длине массива не занося четких цифр
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+
 int[] array = new int[10]; //переменная массива = создай новый массив из 10 элементов (важно понимать он пустой будет)
 
 FillArray(array); //запустили функцию которая заполнила нам массив случайными числами
 PrintArray(array); //запустили функцию которая переберая массив по индексу через Console.WriteLine(col[index]) нам всё вывела
+Console.WriteLine();
+
+int pos = IndexOf(array, 4); //делаем переменную в которую кладём результат работы функции (в качестве аргумента передаём)
+Console.WriteLine(pos); //показываем что сделала функция в терминал
