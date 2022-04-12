6d3743fd94cdba2f5b4c00d61d32fb5eac4546ad
Suxarik777 <gultcev@mail.ru>
2022-04-12 23:18:15 +0300

    Example011, метод заполнения массива, вывода информации на экран массива, создание фиксированного массива

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int[] array = new int[10]; //переменная массива = создай новый массив из 10 элементов (важно понимать он пустой будет)
+
+FillArray(array); //запустили функцию которая заполнила нам массив случайными числами
+PrintArray(array); //запустили функцию которая переберая массив по индексу через Console.WriteLine(col[index]) нам всё вывела
