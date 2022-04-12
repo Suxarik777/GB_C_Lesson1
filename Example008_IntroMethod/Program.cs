ffcb9970ff72e5f59acac7daf910186ea6f588c2
Suxarik777 <gultcev@mail.ru>
2022-04-12 19:39:57 +0300

    Функция с последовательным поиском

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,23 +1,27 @@
-﻿int a1 = 15;
+﻿int MaxFunction(int arg1, int arg2, int arg3) //здесь мы задаём функцию 
+{                               //принимаются данные в скобки в том же порядке как ниже в коде (a1,b1,c1)  
+    int result = arg1;          //и им присваивается (arg1, int arg2, int arg3)  
+    if(arg2 > result) result = arg2;    //(int - помни это просто тип данных (контейнер) целочисленный) 
+    if(arg3 > result) result = arg3;    //просто его мы решили указать в функции
+    return result;
+}
+
+int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
 int b2 = 2311;
 int c2 = 33;
 int a3 = 13;
-int b3 = 23;
+int b3 = 23111;
 int c3 = 313;
 
-int max = a1;
-if(b1 > max) max = b1;
-if(c1 > max) max = c1;
+int max1 = MaxFunction(a1,b1,c1); //MaxFunction(берёт сюда информацию из указанных в переменных a,b,c)
+int max2 = MaxFunction(a2,b2,c2);   //MaxFunction - это название, можно дать любое, например в лекции просто MAX
+int max3 = MaxFunction(a3,b3,c3);
 
-if(a2 > max) max = a2;
-if(b2 > max) max = b2;
-if(c2 > max) max = c2;
+int max = MaxFunction(max1,max2,max3);
 
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
+
 
 Console.WriteLine(max);
